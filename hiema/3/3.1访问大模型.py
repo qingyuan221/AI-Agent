a0ee5400d9dd3cc8162831f6ce3ec97ebb152778
qingyuan221 <2028331378@qq.com>
2026-05-14 14:15:50 +0800

    Delete hiema directory

--- hiema/3/3.1访问大模型.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.llms.tongyi import Tongyi
-model = Tongyi(
-    api_key="",
-    model="qwen-max",
-)
-res = model.invoke("你是谁能干什么")
-print (res)
--- hiema/3/3.5调用嵌入式模型.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.embeddings import DashScopeEmbeddings
-
-model = DashScopeEmbeddings(
-    dashscope_api_key="",
-)
-
-print(model.embed_query("你爱我"))
-print(model.embed_documents("你爱我，我爱你，蜜雪冰城甜蜜蜜"))
-
